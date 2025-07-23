40c5115e406201792bf23a59f8c0742ca43bdd61
Nugitha Disas <nugitha.c@gmail.com>
2025-07-23 22:08:48 +0530

    T-Shirts Section

--- main.c
+++ main.c
@@ -21,4 +21,36 @@ int main(void) {
     customerName[strcspn(customerName, "\n")] = 0; // Remove newline
 
     printf("\n===========================================================================\n\n");
+
+
+    printf("  _______     _____ _     _      _       \n");
+    printf(" |__   __|   / ____| |   (_)    | |      \n");
+    printf("    | |_____| (___ | |__  _ _ __| |_ ___ \n");
+    printf("    | |______\\___ \\| '_ \\| | '__| __/ __|\n");
+    printf("    | |      ____) | | | | | |  | |_\\__ \\\n");
+    printf("    |_|     |_____/|_| |_|_|_|   \\__|___/\n");
+    printf("\n");
+
+    int tQTY;
+    double tShirtUPrice, tShirtDiscount;
+
+    printf("Number of T-Shirts: ");
+    scanf("%d", &tQTY);
+
+    printf("Unit Price of T-Shirt: ");
+    scanf("%lf", &tShirtUPrice);
+
+    printf("Discount Rate (%%): ");
+    scanf("%lf", &tShirtDiscount);
+
+    double tDis = tShirtUPrice * tQTY * tShirtDiscount / 100.0;
+    double tAmount = (tShirtUPrice * tQTY) - tDis;
+
+    printf("\n+---------------------+-----------+-----------------+-----------------+\n");
+    printf("|  Description        |    QTY    |   Unit Price    |      Amount     |\n");
+    printf("+---------------------+-----------+-----------------+-----------------+\n");
+    printf("|   T-Shirts          |%10d |%16.2f |%16.2f |\n", tQTY, tShirtUPrice, tAmount);
+    printf("+---------------------+-----------+-----------------+-----------------+\n");
+
+    printf("\n===========================================================================\n\n");
 }
